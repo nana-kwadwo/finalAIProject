ead48a55265259fcfce73ea0519a253b0a5dde2b
nana-kwadwo <54313306+nana-kwadwo@users.noreply.github.com>
2024-08-04 18:06:52 -0700

    Update app_date.py

--- app_date.py
+++ app_date.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
-from tensorflow.keras.models import load_model
+
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
